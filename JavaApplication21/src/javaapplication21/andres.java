d1efec48d5ba6cff54e25d4d18b51e170485ee62
AndresTapia <andres.tapia@hotmail.com.ar>
2023-03-31 13:45:25 -0300

    Metiendo los archivos de java

---
+++ JavaApplication21/src/javaapplication21/andres.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication21;
+
+/**
+ *
+ * @author andre
+ */
+public class andres {
+        public static void main(String[] args) {
+            System.out.println("yo voy para el repositorio remoto");
+        }
+
+    
+}
